a6c535d5e671468b754ee1e73c6c3690baefed3a
Emiliano G. Garello <emiliano.garello@gmail.com>
2023-06-28 19:54:45 -0300

    git push

---
+++ myMedicalapointment/Main.java
@@ -0,0 +1,13 @@
+package myMedicalapointment;
+
+import static UI.UImenu.*;
+
+import UI.UImenu;
+
+public class Main {
+    public static void main(String[] args) {
+
+        showMenu();
+    }
+
+}
